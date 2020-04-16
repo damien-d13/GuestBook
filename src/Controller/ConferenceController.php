0c141587a40f1d20d53ca07775a5c03381f0f992
Damien <devoti.damien@gmail.com>
2020-04-16 22:24:20 +0200

    Add Page and link Conference

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,28 +2,39 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function index()
+    public function index(Environment $twig, ConferenceRepository $conferenceRepository)
     {
 
 
         return new Response(
-            <<<EOF
-                <html>
-                    <body>
-                <img src="/images/under-construction.gif" />
-                    </body>
-                </html>
-                EOF
+            $twig->render('conference/index.html.twig', [
+                'conferences' => $conferenceRepository->findAll(),
+            ])
         );
+    }
+
+    /**
+     * @Route("/conference/{id}", name="conference")
+     */
+    public function show(Environment $twig, Conference $conference, CommentRepository $commentRepository)
+    {
+        return new Response($twig->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments' => $commentRepository->findBy(['conference' => $conference], ['createdAt' => 'DESC']),
+           
+        ]));
     }
 }
